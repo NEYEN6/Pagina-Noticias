deadc51186806fe16e9e8025caee3cd3f3891900
Sebastián Neyen Frezzini <sebasfrezzini@gmail.com>
2021-10-29 12:52:59 -0300

    Se agrega el Proyecto

---
+++ main.js
@@ -0,0 +1,8 @@
+document.querySelector('.menu-btn').addEventListener('click', () => {
+    document.querySelector('.nav-menu').classList.toggle('show');
+});
+
+ScrollReveal().reveal('.showcase');
+ScrollReveal().reveal('.new-cards', { dalay: 500 });
+ScrollReveal().reveal('.cards-banner-one', { dalay: 500 });
+ScrollReveal().reveal('.cards-banner-two', { dalay: 500 });
